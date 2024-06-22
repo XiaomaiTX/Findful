f4b7f1db73a58a488f9d11557e8e8398fdaf052c
XiaomaiTX <i@lenrome.cn>
2024-06-22 13:27:34 +0800

    fix: LocalStorage

--- page/gt/Finding/index.page.js
+++ page/gt/Finding/index.page.js
@@ -5,7 +5,7 @@ import { Fx } from "../../../libs/fx";
 import VisLog from "@silver-zepp/vis-log";
 import { fpush, pageInit, fback } from "../../../libs/zeppos-fluent-push";
 import { setScrollLock } from "@zos/page";
-import { sessionStorage } from "@zos/storage";
+import { SessionStorage } from "@zos/storage";
 //import BLEMaster from "../../../libs/ble-master";
 import BLEMaster from "@silver-zepp/easy-ble";
 import { setPageBrightTime } from "@zos/display";
@@ -15,106 +15,116 @@ import * as ANIM_STYLE from "./animations";
 import { ZeppAnim } from "../../../libs/zeppos-animation-player";
 
 import {
-	onGesture,
-	GESTURE_UP,
-	GESTURE_DOWN,
-	GESTURE_LEFT,
-	GESTURE_RIGHT,
+    onGesture,
+    GESTURE_UP,
+    GESTURE_DOWN,
+    GESTURE_LEFT,
+    GESTURE_RIGHT,
 } from "@zos/interaction";
 
 const logger = Logger.getLogger("homepage");
 const BLE = new BLEMaster();
 const vis = new VisLog("Finding.js");
+const zosStorage = new SessionStorage();
 
 Page({
-	onInit() {
-		logger.debug("page onInit invoked");
-	},
-	build() {
-		pageInit({
-			onStop() {
-				logger.debug("page build invoked");
-				const ScrollY = getScrollTop();
-				logger.log(ScrollY);
-				setPageBrightTime({ brightTime: 600000 });
+    onInit() {
+        logger.debug("page onInit invoked");
+    },
+    build() {
+        pageInit({
+            onStop() {
+                try {
+                    logger.debug("page build invoked");
+                    const ScrollY = getScrollTop();
+                    logger.log(ScrollY);
+                    setPageBrightTime({ brightTime: 600000 });
 
-				setScrollLock({
-					lock: true,
-				});
-				hmUI.createWidget(hmUI.widget.IMG, STYLE.ICON_BG_STYLE);
-				hmUI.createWidget(hmUI.widget.IMG, STYLE.ICON_STYLE);
-				hmUI.createWidget(hmUI.widget.TEXT, {
-					...STYLE.DEVICE_NAME_STYLE,
-					text: sessionStorage.getItem("dev_name"),
-				});
-				hmUI.createWidget(hmUI.widget.TEXT, {
-					...STYLE.MAC_TEXT_STYLE,
-					text: sessionStorage.getItem("mac"),
-				});
-				const rssiText = hmUI.createWidget(hmUI.widget.TEXT, {
-					...STYLE.DISTANCE_TEXT_STYLE,
-					text: "- m",
-				});
-				hmUI.createWidget(hmUI.widget.BUTTON, {
-					...STYLE.BUTTON_STYLE,
-					click_func: (button_widget) => {
-						BLE.stopScan();
-						new ZeppAnim({
-							tracks: [
-								ANIM_STYLE.TRACK_1,
-								ANIM_STYLE.TRACK_2,
-								ANIM_STYLE.TRACK_3,
-							],
-						});
-					},
-				});
-				var color = 0xffffff;
+                    setScrollLock({
+                        lock: true,
+                    });
+                    hmUI.createWidget(hmUI.widget.IMG, STYLE.ICON_BG_STYLE);
+                    hmUI.createWidget(hmUI.widget.IMG, STYLE.ICON_STYLE);
+                    hmUI.createWidget(hmUI.widget.TEXT, {
+                        ...STYLE.DEVICE_NAME_STYLE,
+                        text: zosStorage.getItem("dev_name"),
+                    });
+                    hmUI.createWidget(hmUI.widget.TEXT, {
+                        ...STYLE.MAC_TEXT_STYLE,
+                        text: zosStorage.getItem("mac"),
+                    });
 
-				function rssi2distance(rssi, m, n) {
-					d = Math.pow(10, (-rssi - m) / (10 * n)).toFixed(2);
-					return d;
-				}
-				// start scanning for nearby devices
-				const scanOptions = {
-					//duration: 4000,
-					throttle_interval: 100,
-					allow_duplicates: true,
-					//on_duration: () => {
-					// 	logger.log("scan stop");
-					//},
-				};
+                    const rssiText = hmUI.createWidget(hmUI.widget.TEXT, {
+                        ...STYLE.DISTANCE_TEXT_STYLE,
+                        text: "- m",
+                    });
+                    hmUI.createWidget(hmUI.widget.BUTTON, {
+                        ...STYLE.BUTTON_STYLE,
+                        click_func: (button_widget) => {
+                            BLE.stopScan();
+                            new ZeppAnim({
+                                tracks: [
+                                    ANIM_STYLE.TRACK_1,
+                                    ANIM_STYLE.TRACK_2,
+                                    ANIM_STYLE.TRACK_3,
+                                ],
+                            });
+                        },
+                    });
+                    var color = 0xffffff;
 
-				const scan_success = BLE.startScan((scan_result) => {
-					// if THE device (mac) that we search for is found
+                    function rssi2distance(rssi, m, n) {
+                        d = Math.pow(10, (-rssi - m) / (10 * n)).toFixed(2);
+                        return d;
+                    }
+                    // start scanning for nearby devices
+                    const scanOptions = {
+                        //duration: 4000,
+                        throttle_interval: 100,
+                        allow_duplicates: true,
+                        //on_duration: () => {
+                        // 	logger.log("scan stop");
+                        //},
+                    };
 
-					if (BLE.get.hasDevice(sessionStorage.getItem("mac"))) {
-						const rssi = parseInt(
-							BLE.get.devices()[sessionStorage.getItem("mac")]
-								.rssi
-						);
-						const distance = rssi2distance(parseInt(rssi), 86, 6);
-						// Todo | Check m and n
-						if (distance >= 0 && distance < 1) {
-							color = 0x00e676;
-						} else if (distance >= 1 && distance < 1.5) {
-							color = 0xffea00;
-						} else if (distance >= 1.5 && distance < 2) {
-							color = 0xff9100;
-						} else if (distance >= 2 && distance < 3.7) {
-							color = 0xff1744;
-						}
+                    const scan_success = BLE.startScan((scan_result) => {
+                        // if THE device (mac) that we search for is found
 
-						rssiText.setProperty(hmUI.prop.MORE, {
-							text: distance + " m",
-							color: color,
-						});
-					}
-				}, scanOptions);
-			},
-		});
-	},
-	onDestroy() {
-		logger.debug("page onDestroy invoked");
-		BLE.stopScan();
-	},
+                        if (BLE.get.hasMAC(zosStorage.getItem("mac"))) {
+                            const rssi = parseInt(
+                                BLE.get.devices()[zosStorage.getItem("mac")]
+                                    .rssi
+                            );
+                            const distance = rssi2distance(
+                                parseInt(rssi),
+                                86,
+                                6
+                            );
+                            // Todo | Check m and n
+                            if (distance >= 0 && distance < 1) {
+                                color = 0x00e676;
+                            } else if (distance >= 1 && distance < 1.5) {
+                                color = 0xffea00;
+                            } else if (distance >= 1.5 && distance < 2) {
+                                color = 0xff9100;
+                            } else if (distance >= 2 && distance < 3.7) {
+                                color = 0xff1744;
+                            }
+
+                            rssiText.setProperty(hmUI.prop.MORE, {
+                                text: distance + " m",
+                                color: color,
+                            });
+                        }
+                    }, scanOptions);
+                } catch (e) {
+                    vis.log(e);
+                }
+            },
+        });
+    },
+    onDestroy() {
+        logger.debug("page onDestroy invoked");
+        BLE.stopScan();
+    },
 });
--- page/gt/HomePage/index.page.js
+++ page/gt/HomePage/index.page.js
@@ -6,7 +6,7 @@ import VisLog from "@silver-zepp/vis-log";
 import BLEMaster from "@silver-zepp/easy-ble";
 import { fpush, pageInit } from "../../../libs/zeppos-fluent-push";
 import { TEST_BUTTON_STYLE, TEST_CONTAINER_STYLE } from "./index.page.r.layout";
-import { SessionStorage } from '@zos/storage'
+import { SessionStorage } from "@zos/storage";
 // import Fx and fxpush
 import * as zosInteraction from "@zos/interaction";
 import * as zosRouter from "@zos/router";
@@ -16,261 +16,265 @@ import * as ANIM_STYLE from "./animations";
 
 const logger = Logger.getLogger("homepage");
 const BLE = new BLEMaster();
-const zosStorage = new SessionStorage()
+const zosStorage = new SessionStorage();
 
 Page({
-	onInit() {
-		logger.debug("page onInit invoked");
-	},
-	build() {
-		pageInit({
-			onStop() {
-				setPageBrightTime({ brightTime: 600000 });
-				zosInteraction.onGesture({
-					callback: (event) => {
-						if (event === zosInteraction.GESTURE_RIGHT) {
-							zosRouter.exit();
-						}
-						return true;
-					},
-				});
-				const vis = new VisLog("HomePage.js");
-				vis.updateSettings({ line_count: 20 });
+    onInit() {
+        logger.debug("page onInit invoked");
+    },
+    build() {
+        pageInit({
+            onStop() {
+                setPageBrightTime({ brightTime: 600000 });
+                zosInteraction.onGesture({
+                    callback: (event) => {
+                        if (event === zosInteraction.GESTURE_RIGHT) {
+                            zosRouter.exit();
+                        }
+                        return true;
+                    },
+                });
+                const vis = new VisLog("HomePage.js");
+                vis.updateSettings({ line_count: 20 });
 
-				//vis.debug("page build invoked");
-				// TODO | Draw UI face
+                //vis.debug("page build invoked");
+                // TODO | Draw UI face
 
-				const background = hmUI.createWidget(
-					hmUI.widget.IMG,
-					STYLE.BG_STYLE
-				);
-				const icon = hmUI.createWidget(
-					hmUI.widget.IMG,
-					STYLE.ICON_STYLE
-				);
-				const title = hmUI.createWidget(
-					hmUI.widget.TEXT,
-					STYLE.TITLE_STYLE
-				);
-				const describtionText = hmUI.createWidget(
-					hmUI.widget.TEXT,
-					STYLE.DESCRIBTION_STYLE
-				);
-				const subtitle = hmUI.createWidget(
-					hmUI.widget.TEXT,
-					STYLE.SUBTITLE_STYLE
-				);
-				new ZeppAnim({
-					tracks: [ANIM_STYLE.TRACK_1],
-				});
-				// TODO | Animation of progress bar
-				icon.addEventListener(hmUI.event.CLICK_DOWN, function (info) {
-					BLE.stopScan();
-					if (_devicesListUIGroup) {
-						hmUI.deleteWidget(_devicesListUIGroup);
-						// logger.log("delete _devicesListUIGroup");
-						hmUI.redraw();
-					}
+                const background = hmUI.createWidget(
+                    hmUI.widget.IMG,
+                    STYLE.BG_STYLE
+                );
+                const icon = hmUI.createWidget(
+                    hmUI.widget.IMG,
+                    STYLE.ICON_STYLE
+                );
+                const title = hmUI.createWidget(
+                    hmUI.widget.TEXT,
+                    STYLE.TITLE_STYLE
+                );
+                const describtionText = hmUI.createWidget(
+                    hmUI.widget.TEXT,
+                    STYLE.DESCRIBTION_STYLE
+                );
+                const subtitle = hmUI.createWidget(
+                    hmUI.widget.TEXT,
+                    STYLE.SUBTITLE_STYLE
+                );
+                new ZeppAnim({
+                    tracks: [ANIM_STYLE.TRACK_1],
+                });
+                // TODO | Animation of progress bar
+                icon.addEventListener(hmUI.event.CLICK_DOWN, function (info) {
+                    BLE.stopScan();
+                    if (_devicesListUIGroup) {
+                        hmUI.deleteWidget(_devicesListUIGroup);
+                        // logger.log("delete _devicesListUIGroup");
+                        hmUI.redraw();
+                    }
 
-					fpush({
-						url: "page/gt/About/index.page",
-						params: "",
-					});
-				});
-				// TODO | Search for BLE devices
-				var rawDevices = [];
-				//logger.log("init rawDevices");
-				var sortedDevices = [];
-				function rssi2distance(rssi, m, n) {
-					d = Math.pow(10, (-rssi - m) / (10 * n)).toFixed(1);
-					return d;
-				}
+                    fpush({
+                        url: "page/gt/About/index.page",
+                        params: "",
+                    });
+                });
+                // TODO | Search for BLE devices
+                var rawDevices = [];
+                //logger.log("init rawDevices");
+                var sortedDevices = [];
+                function rssi2distance(rssi, m, n) {
+                    d = Math.pow(10, (-rssi - m) / (10 * n)).toFixed(1);
+                    return d;
+                }
 
-				logger.log("Init scan");
-				function bleScan() {
-					// Start scan | stop scan 4s later | return sortedDevices 5s later
-					const scanOptions = {
-						//duration: 4000,
-						throttle_interval: 100,
-						allow_duplicates: true,
-						//on_duration: () => {
-						// 	logger.log("scan stop");
-						//},
-					};
+                logger.log("Init scan");
+                function bleScan() {
+                    // Start scan | stop scan 4s later | return sortedDevices 5s later
+                    const scanOptions = {
+                        //duration: 4000,
+                        throttle_interval: 100,
+                        allow_duplicates: true,
+                        //on_duration: () => {
+                        // 	logger.log("scan stop");
+                        //},
+                    };
 
-					BLE.startScan((scan_result) => {
-						if (scan_result) {
-							// Scan success
-							// logger.log("Scan success");
-							Object.keys(BLE.get.devices()).forEach((mac) => {
-								const dev_name =
-									BLE.get.devices()[mac].dev_name;
-								if (dev_name !== "unnamed_device") {
-									// Delete unnamed device
-									const rssi = BLE.get.devices()[mac].rssi;
+                    BLE.startScan((scan_result) => {
+                        if (scan_result) {
+                            // Scan success
+                            // logger.log("Scan success");
+                            Object.keys(BLE.get.devices()).forEach((mac) => {
+                                const dev_name =
+                                    BLE.get.devices()[mac].dev_name;
+                                if (dev_name !== "unnamed_device") {
+                                    // Delete unnamed device
+                                    const rssi = BLE.get.devices()[mac].rssi;
 
-									rawDevices.push({ mac, dev_name, rssi });
-								}
-							});
-						} else {
-							// Scan failed
-							logger.log("Scan failed");
-							BLE.stopScan();
-						}
-					}, scanOptions);
+                                    rawDevices.push({ mac, dev_name, rssi });
+                                }
+                            });
+                        } else {
+                            // Scan failed
+                            logger.log("Scan failed");
+                            BLE.stopScan();
+                        }
+                    }, scanOptions);
 
-					//BLE.stopScan();
-					return 0;
-				}
-				function DrawDevicesList(sortedDevices, _devicesListUIGroup) {
-					// TODO | Refresh scan results
-					// logger.log("Draw"+rawDevices.length);
-					// Delete old UI
-					logger.log("========================");
-					// Delete duplicate devices
-					const uniqueDevices = Array.from(
-						new Set(rawDevices.map((device) => device.mac))
-					).map((mac) =>
-						rawDevices.find((device) => device.mac === mac)
-					);
-					// Sort by RSSI (-20 ~ -100 dB)
+                    //BLE.stopScan();
+                    return 0;
+                }
+                function DrawDevicesList(sortedDevices, _devicesListUIGroup) {
+                    // TODO | Refresh scan results
+                    // logger.log("Draw"+rawDevices.length);
+                    // Delete old UI
+                    logger.log("========================");
+                    // Delete duplicate devices
+                    const uniqueDevices = Array.from(
+                        new Set(rawDevices.map((device) => device.mac))
+                    ).map((mac) =>
+                        rawDevices.find((device) => device.mac === mac)
+                    );
+                    // Sort by RSSI (-20 ~ -100 dB)
 
-					sortedDevices = uniqueDevices.sort(
-						(a, b) => b.rssi - a.rssi
-					);
+                    sortedDevices = uniqueDevices.sort(
+                        (a, b) => b.rssi - a.rssi
+                    );
 
-					sortedDevices.forEach((device) => {
-						logger.log(
-							`${device.mac} - ${device.dev_name} - ${device.rssi}`
-						);
-					});
+                    sortedDevices.forEach((device) => {
+                        logger.log(
+                            `${device.mac} - ${device.dev_name} - ${device.rssi}`
+                        );
+                    });
 
-					if (_devicesListUIGroup) {
-						hmUI.deleteWidget(_devicesListUIGroup);
-						// logger.log("delete _devicesListUIGroup");
-						hmUI.redraw();
-					}
-					// logger.log("Draw UI");
-					const devicesListUIGroup = hmUI.createWidget(
-						hmUI.widget.GROUP,
-						{
-							...STYLE.DEVICES_LIST_UI_GROUP_STYLE,
-							h:
-								STYLE.ITEM_CONTAINER_STYLE.y +
-								(STYLE.ITEM_CONTAINER_STYLE.h + px(20)) *
-									(sortedDevices.length + 1),
-						}
-					);
+                    if (_devicesListUIGroup) {
+                        hmUI.deleteWidget(_devicesListUIGroup);
+                        // logger.log("delete _devicesListUIGroup");
+                        hmUI.redraw();
+                    }
+                    // logger.log("Draw UI");
+                    const devicesListUIGroup = hmUI.createWidget(
+                        hmUI.widget.GROUP,
+                        {
+                            ...STYLE.DEVICES_LIST_UI_GROUP_STYLE,
+                            h:
+                                STYLE.ITEM_CONTAINER_STYLE.y +
+                                (STYLE.ITEM_CONTAINER_STYLE.h + px(20)) *
+                                    (sortedDevices.length + 1),
+                        }
+                    );
 
-					_devicesListUIGroup = devicesListUIGroup;
-					sortedDevices.forEach((device, index) => {
-						// Draw devices list UI
-						devicesListUIGroup
-							.createWidget(hmUI.widget.IMG, {
-								...STYLE.ITEM_CONTAINER_STYLE,
-								y:
-									STYLE.ITEM_CONTAINER_STYLE.y +
-									(STYLE.ITEM_CONTAINER_STYLE.h + px(20)) *
-										index,
-							})
-							.addEventListener(
-								hmUI.event.CLICK_UP,
-								function (info) {
-									if (_devicesListUIGroup) {
-										hmUI.deleteWidget(_devicesListUIGroup);
-										// logger.log(
-										// 	"delete _devicesListUIGroup"
-										// );
-										hmUI.redraw();
-									}
+                    _devicesListUIGroup = devicesListUIGroup;
+                    sortedDevices.forEach((device, index) => {
+                        // Draw devices list UI
+                        devicesListUIGroup
+                            .createWidget(hmUI.widget.IMG, {
+                                ...STYLE.ITEM_CONTAINER_STYLE,
+                                y:
+                                    STYLE.ITEM_CONTAINER_STYLE.y +
+                                    (STYLE.ITEM_CONTAINER_STYLE.h + px(20)) *
+                                        index,
+                            })
+                            .addEventListener(
+                                hmUI.event.CLICK_UP,
+                                function (info) {
+                                    try {
+                                        zosStorage.setItem(
+                                            "dev_name",
+                                            device.dev_name
+                                        );
+                                        zosStorage.setItem(
+                                            "mac",
+                                            device.mac
+                                        );
+                                        zosStorage.setItem(
+                                            "rssi",
+                                            device.rssi
+                                        );
 
-									zosStorage.sessionStorage.setItem(
-										"dev_name",
-										device.dev_name
-									);
-									zosStorage.sessionStorage.setItem(
-										"mac",
-										device.mac
-									);
-									zosStorage.sessionStorage.setItem(
-										"rssi",
-										device.rssi
-									);
+                                        BLE.stopScan();
 
-									BLE.stopScan();
-									fpush({
-										url: "page/gt/Finding/index.page",
-										params: "",
-									});
-								}
-							); //*/
-						devicesListUIGroup
-							.createWidget(hmUI.widget.TEXT, {
-								...STYLE.ITEM_TITLE_STYLE,
-								y:
-									STYLE.ITEM_TITLE_STYLE.y +
-									(STYLE.ITEM_CONTAINER_STYLE.h + px(20)) *
-										index,
-								text: device.dev_name,
-							})
-							.setEnable(false);
-						devicesListUIGroup
-							.createWidget(hmUI.widget.IMG, {
-								...STYLE.ITEM_ICON_STYLE,
-								y:
-									STYLE.ITEM_ICON_STYLE.y +
-									(STYLE.ITEM_CONTAINER_STYLE.h + px(20)) *
-										index,
-							})
-							.setEnable(false);
-						devicesListUIGroup
-							.createWidget(hmUI.widget.TEXT, {
-								...STYLE.ITEM_DESTANCE_TEXT_STYLE,
-								y:
-									STYLE.ITEM_DESTANCE_TEXT_STYLE.y +
-									(STYLE.ITEM_CONTAINER_STYLE.h + px(20)) *
-										index,
-								text: rssi2distance(device.rssi, 86, 6) + " m",
-							})
-							.setEnable(false);
-						devicesListUIGroup
-							.createWidget(hmUI.widget.TEXT, {
-								...STYLE.ITEM_MAC_TEXT_STYLE,
-								y:
-									STYLE.ITEM_MAC_TEXT_STYLE.y +
-									(STYLE.ITEM_CONTAINER_STYLE.h + px(20)) *
-										index,
-								text: device.mac,
-							})
-							.setEnable(false);
-					});
-					// logger.log("recreate UI");
-					rawDevices.length = null;
-					// logger.log("rawDevices.length " + rawDevices.length);
+                                        fpush({
+                                            url: "page/gt/Finding/index.page",
+                                            params: "",
+                                        });
+                                    } catch (e) {
+                                        vis.log(e);
+                                    }
+                                    if (_devicesListUIGroup) {
+                                        hmUI.deleteWidget(_devicesListUIGroup);
+                                        // logger.log(
+                                        // 	"delete _devicesListUIGroup"
+                                        // );
+                                        hmUI.redraw();
+                                    }
+                                }
+                            ); //*/
+                        devicesListUIGroup
+                            .createWidget(hmUI.widget.TEXT, {
+                                ...STYLE.ITEM_TITLE_STYLE,
+                                y:
+                                    STYLE.ITEM_TITLE_STYLE.y +
+                                    (STYLE.ITEM_CONTAINER_STYLE.h + px(20)) *
+                                        index,
+                                text: device.dev_name,
+                            })
+                            .setEnable(false);
+                        devicesListUIGroup
+                            .createWidget(hmUI.widget.IMG, {
+                                ...STYLE.ITEM_ICON_STYLE,
+                                y:
+                                    STYLE.ITEM_ICON_STYLE.y +
+                                    (STYLE.ITEM_CONTAINER_STYLE.h + px(20)) *
+                                        index,
+                            })
+                            .setEnable(false);
+                        devicesListUIGroup
+                            .createWidget(hmUI.widget.TEXT, {
+                                ...STYLE.ITEM_DESTANCE_TEXT_STYLE,
+                                y:
+                                    STYLE.ITEM_DESTANCE_TEXT_STYLE.y +
+                                    (STYLE.ITEM_CONTAINER_STYLE.h + px(20)) *
+                                        index,
+                                text: rssi2distance(device.rssi, 86, 6) + " m",
+                            })
+                            .setEnable(false);
+                        devicesListUIGroup
+                            .createWidget(hmUI.widget.TEXT, {
+                                ...STYLE.ITEM_MAC_TEXT_STYLE,
+                                y:
+                                    STYLE.ITEM_MAC_TEXT_STYLE.y +
+                                    (STYLE.ITEM_CONTAINER_STYLE.h + px(20)) *
+                                        index,
+                                text: device.mac,
+                            })
+                            .setEnable(false);
+                    });
+                    // logger.log("recreate UI");
+                    rawDevices.length = null;
+                    // logger.log("rawDevices.length " + rawDevices.length);
 
-					sortedDevices.length = null;
-					// logger.log("sortedDevices.length " + sortedDevices.length);
+                    sortedDevices.length = null;
+                    // logger.log("sortedDevices.length " + sortedDevices.length);
 
-					return devicesListUIGroup;
-				}
-				bleScan();
-				const scanDevicesTimer = setInterval(() => {
-					switch (pageStatus) {
-						case "scan_for_more_devices":
-						case "scan_for_the_one_device":
-					}
+                    return devicesListUIGroup;
+                }
+                bleScan();
+                const scanDevicesTimer = setInterval(() => {
+                    switch (pageStatus) {
+                        case "scan_for_more_devices":
+                        case "scan_for_the_one_device":
+                    }
 
-					_devicesListUIGroup = DrawDevicesList(
-						sortedDevices,
-						_devicesListUIGroup
-					);
-				}, 3000);
-			},
-		});
-	},
-	onDestroy() {
-		logger.debug("page onDestroy invoked");
-		zosInteraction.offGesture();
-		BLE.stopScan();
-	},
+                    _devicesListUIGroup = DrawDevicesList(
+                        sortedDevices,
+                        _devicesListUIGroup
+                    );
+                }, 3000);
+            },
+        });
+    },
+    onDestroy() {
+        logger.debug("page onDestroy invoked");
+        zosInteraction.offGesture();
+        BLE.stopScan();
+    },
 });
